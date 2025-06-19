014fc85d31c222d41c5b3f0cc67d43240acbaed8
Hong Seoung Jin <116872259+RedPromotion@users.noreply.github.com>
2025-06-19 16:41:54 +0900

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 // Vuetify 관련 import
+// @ts-ignore
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
